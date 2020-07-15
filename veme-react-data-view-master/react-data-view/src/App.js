1906d42949eff168ca636065f8923de3fd00a8b0
张天琪 <3039878055@qq.com>
2020-07-15 14:49:29 +0800

    提交

---
+++ veme-react-data-view-master/react-data-view/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import "./App.css"
+import DemoOne from "./components/demoone/DemoOne"
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <DemoOne></DemoOne>
+      </div>
+    )
+  }
+}
